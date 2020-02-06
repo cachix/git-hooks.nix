8fe8355c62765c1d6a7ba4bd3247277782a4d3f3
Domen Kožar <domen@dev.si>
2020-02-06 11:00:14 +0100

    simplify

--- nix/default.nix
+++ nix/default.nix
@@ -2,37 +2,31 @@
 , system ? builtins.currentSystem
 }:
 
-with {
-    haskellnix = import sources."haskell.nix";
-    overlay =
-      _: pkgs:
-        let
-          pkgPlan =
-            pkgs.haskell-nix.callCabalProjectToNix {
-              #index-state = "2020-02-18T00:00:00Z";
-              src = sources.cabal-fmt;
-            };
-          # Instantiate a package set using the generated file.
-          pkgSet =
-            pkgs.haskell-nix.mkCabalProjectPkgSet {
-              plan-pkgs = import pkgPlan;
-              pkg-def-extras = [];
-              modules = [];
-            };
-        in {
-          inherit (pkgs) nixfmt niv ormolu nixpkgs-fmt;
-          hindent =
-            pkgs.haskellPackages.callCabal2nix "hindent" sources.hindent {};
-          # TODO: expose overlay to avoid evaluating nixpkgs twice
-          inherit (import sources.canonix {}) canonix;
-          # Requires Cabal 3, wait for LTS 15
-          cabal-fmt =
-            pkgSet.config.hsPkgs.cabal-fmt.components.exes.cabal-fmt;
-          packages = pkgs.callPackages ./packages.nix {};
-        };
-  };
-import sources.nixpkgs {
-  overlays = haskellnix.overlays ++ [ overlay ];
-  config = haskellnix.config // {};
-  inherit system;
-}
+let
+  haskellnix = import sources."haskell.nix";
+  overlay =
+    _: pkgs:
+      let
+        cabal-fmt =
+          pkgs.haskellPackages.callCabal2nix "cabal-fmt" sources.cabal-fmt {};
+      in {
+        inherit (pkgs) nixfmt niv ormolu nixpkgs-fmt;
+        hindent =
+          pkgs.haskellPackages.callCabal2nix "hindent" sources.hindent {};
+        # TODO: expose overlay to avoid evaluating nixpkgs twice
+        inherit (import sources.canonix {}) canonix;
+        cabal-fmt =
+          cabal-fmt.overrideScope (
+            self: super:
+              {
+                Cabal = self.Cabal_3_0_0_0;
+              }
+          );
+        packages = pkgs.callPackages ./packages.nix {};
+      };
+in
+  import sources.nixpkgs {
+    overlays = haskellnix.overlays ++ [ overlay ];
+    config = haskellnix.config // {};
+    inherit system;
+  }
