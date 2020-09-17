b840cb825611fb144471241325243731c980a9c3
Domen Kožar <domen@dev.si>
2020-09-17 16:02:06 +0200

    use upstream cabal-fmt

--- nix/default.nix
+++ nix/default.nix
@@ -5,23 +5,12 @@
 let
   overlay =
     _: pkgs:
-      let
-        cabal-fmt =
-          pkgs.haskellPackages.callCabal2nix "cabal-fmt" sources.cabal-fmt {};
-      in
-        {
-          inherit (pkgs) nixfmt niv ormolu nixpkgs-fmt nix-linter;
-          hindent =
-            pkgs.haskellPackages.callCabal2nix "hindent" sources.hindent {};
-          cabal-fmt =
-            cabal-fmt.overrideScope (
-              self: super:
-                {
-                  Cabal = self.Cabal_3_0_0_0;
-                }
-            );
-          packages = pkgs.callPackages ./packages.nix {};
-        };
+      {
+        inherit (pkgs) nixfmt niv ormolu nixpkgs-fmt nix-linter;
+        cabal-fmt = pkgs.haskellPackages.cabal-fmt;
+        hindent = pkgs.haskellPackages.callCabal2nix "hindent" sources.hindent {};
+        packages = pkgs.callPackages ./packages.nix {};
+      };
 in
 import sources.nixpkgs {
   overlays = [ overlay ];
