0e11ee8f475a7259e0a048f4ea9c9f8d4dd3e7d2
Domen Kožar <domen@dev.si>
2021-04-26 12:16:58 +0200

    Bump nixpkgs to nixpkgs-unstable
    Gives us fresher tooling and being able to move faster.

--- nix/tools.nix
+++ nix/tools.nix
@@ -16,7 +16,7 @@
 , nixfmt
 , nix-linter
 , callPackage
-, pythonPackages
+, python39Packages
 , rustfmt
 , clippy
 , cargo
@@ -26,10 +26,10 @@
 }:
 
 {
-  inherit ansible-lint hlint shellcheck ormolu hindent cabal-fmt nixpkgs-fmt nixfmt nix-linter rustfmt clippy cargo html-tidy;
+  inherit hlint shellcheck ormolu hindent cabal-fmt nixpkgs-fmt nixfmt nix-linter rustfmt clippy cargo html-tidy;
   inherit (elmPackages) elm-format;
   inherit (haskellPackages) stylish-haskell brittany hpack fourmolu;
-  inherit (pythonPackages) yamllint;
+  inherit (python39Packages) yamllint ansible-lint;
   inherit (nodePackages) prettier;
   purty = callPackage ./purty { purty = nodePackages.purty; };
   terraform-fmt = callPackage ./terraform-fmt { };
