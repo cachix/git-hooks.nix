e08722ae02335f3ef562246e021742825c4f66b8
Robert Hensing <robert@roberthensing.nl>
2023-04-07 12:32:14 +0200

    gptcommit: Improve error when package is missing

--- modules/hooks.nix
+++ modules/hooks.nix
@@ -481,7 +481,9 @@ in
                 "$PRE_COMMIT_COMMIT_MSG_SOURCE" --commit-msg-file "$1"
             '';
           in
-          toString script;
+          lib.throwIf (tools.gptcommit == null) "The version of Nixpkgs used by pre-commit-hooks.nix does not have the `gptcommit` package. Please use a more recent version of Nixpkgs."
+            toString
+            script;
         stages = [ "prepare-commit-msg" ];
       };
       hlint =
