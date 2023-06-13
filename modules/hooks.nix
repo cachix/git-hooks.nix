1d6571590bacc8f62da19d68444a312fed7f2ee9
Niols <niols@niols.fr>
2023-06-13 11:36:41 +0200

    `dune-opam-sync` should also trigger on opam template files

--- modules/hooks.nix
+++ modules/hooks.nix
@@ -570,7 +570,7 @@ in
         name = "dune/opam sync";
         description = "Check that Dune-generated OPAM files are in sync.";
         entry = "${tools.dune-build-opam-files}/bin/dune-build-opam-files";
-        files = "(\\.opam$)|((^|/)dune-project$)";
+        files = "(\\.opam$)|(\\.opam.template$)|((^|/)dune-project$)";
         ## We don't pass filenames because they can only be misleading. Indeed,
         ## we need to re-run `dune build` for every `*.opam` file, but also when
         ## the `dune-project` file has changed.
