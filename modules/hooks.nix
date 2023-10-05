03843c0e6e577e51e840f3e0475c1bb6c48aa559
Matthias Thym <git@thym.at>
2023-10-05 02:08:06 +0200

    Add hook for mdl (Markdown linter)

--- modules/hooks.nix
+++ modules/hooks.nix
@@ -200,6 +200,88 @@ in
             default = { };
           };
       };
+      mdl = {
+        package =
+          mkOption {
+            type = types.package;
+            description = lib.mdDoc "The `mdl` package to use.";
+            default = "${tools.mdl}";
+            defaultText = "\${tools.mdl}";
+            example = "\${pkgs.mdl}";
+          };
+        configPath =
+          mkOption {
+            type = types.str;
+            description = lib.mdDoc "The configuration file to use.";
+            default = "";
+          };
+        git-recurse =
+          mkOption {
+            type = types.bool;
+            description = lib.mdDoc "Only process files known to git when given a directory.";
+            default = false;
+          };
+        ignore-front-matter =
+          mkOption {
+            type = types.bool;
+            description = lib.mdDoc "Ignore YAML front matter.";
+            default = false;
+          };
+        json =
+          mkOption {
+            type = types.bool;
+            description = lib.mdDoc "Format output as JSON.";
+            default = false;
+          };
+        rules =
+          mkOption {
+            type = types.listOf types.str;
+            description = lib.mdDoc "Markdown rules to use for linting. Per default all rules are processed.";
+            default = [ ];
+          };
+        rulesets =
+          mkOption {
+            type = types.listOf types.str;
+            description = lib.mdDoc "Specify additional ruleset files to load.";
+            default = [ ];
+          };
+        show-aliases =
+          mkOption {
+            type = types.bool;
+            description = lib.mdDoc "Show rule alias instead of rule ID when viewing rules.";
+            default = false;
+          };
+        warnings =
+          mkOption {
+            type = types.bool;
+            description = lib.mdDoc "Show Kramdown warnings.";
+            default = false;
+          };
+        skip-default-ruleset =
+          mkOption {
+            type = types.bool;
+            description = lib.mdDoc "Do not load the default markdownlint ruleset. Use this option if you only want to load custom rulesets.";
+            default = false;
+          };
+        style =
+          mkOption {
+            type = types.str;
+            description = lib.mdDoc "Select which style mdl uses.";
+            default = "default";
+          };
+        tags =
+          mkOption {
+            type = types.listOf types.str;
+            description = lib.mdDoc "Markdown rules to use for linting containing the given tags. Per default all rules are processed.";
+            default = [ ];
+          };
+        verbose =
+          mkOption {
+            type = types.bool;
+            description = lib.mdDoc "Increase verbosity.";
+            default = false;
+          };
+      };
       denolint =
         {
           format =
@@ -1558,6 +1640,33 @@ in
           name = "markdownlint";
           description = "Style checker and linter for markdown files.";
           entry = "${tools.markdownlint-cli}/bin/markdownlint -c ${pkgs.writeText "markdownlint.json" (builtins.toJSON settings.markdownlint.config)}";
+          files = "\\.md$";
+        };
+
+      mdl =
+        {
+          name = "mdl";
+          description = "A tool to check markdown files and flag style issues.";
+          entry =
+            let
+              cmdArgs =
+                mkCmdArgs
+                  (with settings.mdl; [
+                    [ (configPath != "") "--config ${configPath}" ]
+                    [ git-recurse "--git-recurse" ]
+                    [ ignore-front-matter "--ignore-front-matter" ]
+                    [ json "--json" ]
+                    [ (rules != [ ]) "--rules ${lib.strings.concatStringsSep "," rules}" ]
+                    [ (rulesets != [ ]) "--rulesets ${lib.strings.concatStringsSep "," rulesets}" ]
+                    [ show-aliases "--show-aliases" ]
+                    [ warnings "--warnings" ]
+                    [ skip-default-ruleset "--skip-default-ruleset" ]
+                    [ (style != "") "--style ${style}" ]
+                    [ (tags != [ ]) "--tags ${lib.strings.concatStringsSep "," tags}" ]
+                    [ verbose "--verbose" ]
+                  ]);
+            in
+            "${settings.mdl.package}/bin/mdl ${cmdArgs}";
           files = "\\.md$";
         };
 
