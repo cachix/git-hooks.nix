35ba4e74d7ad7f3475a1484f3b993ff7a3336aef
Marc Jakobi <marc.jakobi@tiko.energy>
2024-01-10 22:32:13 +0100

    fix(stylua): add --respect-ignores flag

--- modules/hooks.nix
+++ modules/hooks.nix
@@ -2065,7 +2065,7 @@ in
           name = "stylua";
           description = "An Opinionated Lua Code Formatter.";
           types = [ "file" "lua" ];
-          entry = "${tools.stylua}/bin/stylua";
+          entry = "${tools.stylua}/bin/stylua --respect-ignores";
         };
       tagref =
         {
