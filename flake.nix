0b6fdb0941e4dc283ff0e79e2d0aea74d713f2fc
K900 <me@0upti.me>
2024-01-14 01:27:06 +0300

    chore: update to nixos 23.11

--- flake.nix
+++ flake.nix
@@ -2,7 +2,7 @@
   description = "Seamless integration of https://pre-commit.com git hooks with Nix.";
 
   inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
-  inputs.nixpkgs-stable.url = "github:NixOS/nixpkgs/nixos-23.05";
+  inputs.nixpkgs-stable.url = "github:NixOS/nixpkgs/nixos-23.11";
   inputs.flake-utils.url = "github:numtide/flake-utils";
   inputs.flake-compat = {
     url = "github:edolstra/flake-compat";
