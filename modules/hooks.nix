3e3ebb2765e48344a3ab700fc25c125181eb8bcb
Rohit Singh <rsrohitsingh682@gmail.com>
2024-12-21 21:33:15 +0530

    chore(cabal2nix): stick to camelCase

--- modules/hooks.nix
+++ modules/hooks.nix
@@ -213,7 +213,7 @@ in
         type = types.submodule {
           imports = [ hookModule ];
           options.settings = {
-            output_filename =
+            outputFilename =
               mkOption {
                 type = types.str;
                 description = "The name of the output file generated after running `cabal2nix`.";
@@ -2050,7 +2050,7 @@ in
           name = "cabal2nix";
           description = "Run `cabal2nix` on all `*.cabal` files to generate corresponding `.nix` files";
           package = tools.cabal2nix-dir;
-          entry = "${hooks.cabal2nix.package}/bin/cabal2nix-dir --outputFileName=${hooks.cabal2nix.settings.output_filename}";
+          entry = "${hooks.cabal2nix.package}/bin/cabal2nix-dir --outputFileName=${hooks.cabal2nix.settings.outputFilename}";
           files = "\\.cabal$";
           after = [ "hpack" ];
         };
